8698e946b06948e386fd5d60674e9a8026602dd3
fatma arik copur <fatmaarik@gmail.com>
2018-12-05 20:31:33 -0500

    Multilingual greeting repository is added.

--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -13,6 +13,9 @@ import org.springframework.stereotype.Service;
 public class PrimarySpanishGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
+    public PrimarySpanishGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
 
     @Override
     public String sayGreeting() {
